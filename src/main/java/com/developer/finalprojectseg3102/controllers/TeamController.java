325e8e8893d6e1ef5d75b83e25574e2c7c51be5f
JannahH <jhoss094@uottawa.ca>
2019-11-21 17:02:06 -0500

    Added student pages

--- src/main/java/com/developer/finalprojectseg3102/controllers/TeamController.java
+++ src/main/java/com/developer/finalprojectseg3102/controllers/TeamController.java
@@ -1,5 +1,7 @@
 package com.developer.finalprojectseg3102.controllers;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +10,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class TeamController{
 
-	@RequestMapping(value="/team")
-	public String getLoginPage(@RequestParam(name = "username", required = false, defaultValue = "person")
-	String name, Model model) {
-		model.addAttribute("username", name);
-		return "login";
+	@RequestMapping(value="/course")
+	public String getCoursePage(Model model, HttpSession session) {
+		return "course";
 		
 	}
 	
+	@RequestMapping(value="/team")
+	public String getTeamPage(Model model, HttpSession session) {
+		return "team";
+		
+	}
+	
+	@RequestMapping(value="/thread")
+	public String getThreadPage(Model model, HttpSession session) {
+		return "thread";
+		
+	}
 }
