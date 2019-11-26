199870c8140426ff03a5f8d6cf7b5467edfb1c4b
Parasto Saharkhiz <psaha091@uottawa.ca>
2019-11-26 18:00:58 -0500

    retrive user

--- src/main/java/com/developer/finalprojectseg3102/dao/UserDAO.java
+++ src/main/java/com/developer/finalprojectseg3102/dao/UserDAO.java
@@ -2,7 +2,22 @@ package com.developer.finalprojectseg3102.dao;
 
 import java.sql.Connection;
 import java.sql.Statement;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.ArrayList;
 
+import java.util.Scanner;
+
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONObject;
+
+import com.developer.finalprojectseg3102.models.User;
 import com.developer.finalprojectseg3102.models.User;;
 
 public class UserDAO extends BaseDAO {
@@ -51,6 +66,44 @@ public class UserDAO extends BaseDAO {
 		}
 		
 	}
+
+	public static User retrieve(Integer id) throws Exception{
+
+		URL url = new URL("http://team-management-system.herokuapp.com/api/v1/users/" + id.toString()+"/?format=json");
+		HttpURLConnection con = (HttpURLConnection)url.openConnection();
+		con.setRequestMethod("GET");
+		con.connect();
+		int responseCode = con.getResponseCode();
+
+		if(responseCode != 200){
+			throw new RuntimeException("HttpResponseCode: " + responseCode);
+		}
+		else{
+			String rawJson = new String();
+			Scanner sc = new Scanner(url.openStream());
+			while(sc.hasNext()){
+				rawJson += sc.nextLine();
+			}
+			sc.close();
+			JSONParser parse = new JSONParser();
+			JSONObject jsonObj = (JSONObject)parse.parse(rawJson);
+
+			User user = new User();
+
+			// Create this method
+//			user.setUser_id(((Long)jsonObj.get("user_id")).intValue());
+			user.setAccountType((String)jsonObj.get("account_type"));
+			user.setFirstName((String)jsonObj.get("first_name"));
+			user.setLastName((String)jsonObj.get("last_name"));
+			user.setIdentificationNumber((Long)jsonObj.get("indentification_number"));
+			user.setProgram((String)jsonObj.get("program"));
+			user.setEmail((String)jsonObj.get("email"));
+			user.setPassword((String)jsonObj.get("password"));
+
+			return user;
+		}
+	}
+
 	public static void retrieveList() {
 		Connection connection;
 		try {
@@ -67,4 +120,7 @@ public class UserDAO extends BaseDAO {
 		}
 		
 	}
+
+
+
 }
